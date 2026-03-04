2afe45ac6d9fe5fd663369130c91b0a12ffa83d4
hikaru-family <ulfiacans444@gmail.com>
2026-03-04 11:03:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const API_KEY = "MASUKKAN_API_KEY";
-const CHANNEL_ID = "MASUKKAN_CHANNEL_ID";
+const API_KEY = "AIzaSyBMzeKUnFr-SsDu9NUY-Bd2xStqrpaIva0";
+const CHANNEL_ID = "UC4-lIHcs5WDuZ4P-6Bjm7KQ";
 
 fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY}`)
 .then(res => res.json())
